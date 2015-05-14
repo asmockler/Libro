3e1474061db1954618b96ca9e32bbde9dd1f6695
asmockler <asmockler@gmail.com>
2015-05-14 02:32:55 -0600

    Initial Commit

---
+++ make_books.rb
@@ -0,0 +1,131 @@
+require 'prawn'
+require 'json'
+
+class Book
+	attr_accessor :chapters
+
+	def initialize (title, author)
+		@title = title
+		@author = author
+		@chapters = Array.new
+	end
+
+	def add_chapter (chapter)
+		@chapters.push chapter
+	end
+
+	def print
+		puts "\n\n\n#{@title}\n#{@author}"
+		@chapters.each do |c|
+			c.print
+		end
+		puts "\n\n"
+	end
+end
+
+class Chapter
+	@@number = 1
+
+	attr_accessor :text, :chapter_no
+
+	def initialize
+		@chapter_no = @@number
+		@@number += 1
+		@text = Array.new
+	end
+
+	def add_paragraph (text)
+		@text.push text
+	end
+
+	def print
+		puts "\n\nChapter #{@chapter_no}"
+		@text.each do |p|
+			puts "\n#{p}"
+		end
+	end
+end
+
+print "Title: "
+title = gets.chomp
+print "Author: "
+author = gets.chomp
+print "File: "
+file = gets.chomp
+
+book = Book.new(title, author)
+
+chapter = nil
+paragraph = ""
+
+File.readlines(file).each do |line|
+	# This means it is the end of a paragraph
+	if line.length == 1
+		chapter.add_paragraph(paragraph)
+		paragraph = ""
+	# This is the start of a Chapter
+	elsif line[0..6].downcase == "chapter"
+
+		if chapter
+			# Save the preceding chapter
+			chapter.add_paragraph(paragraph)
+			book.add_chapter(chapter)
+		end
+
+		# Start a new chapter
+		chapter = Chapter.new
+		paragraph = ""
+	elsif line["\n"]
+		line = line.gsub(/\n/, " ")
+		paragraph << line
+	end
+end
+
+# Get the last chapter
+chapter.add_paragraph(paragraph)
+book.add_chapter(chapter)
+
+# Print it for testing purposes
+book.print
+# book.chapters.each do |c|
+# 	puts c 
+# end
+
+
+# FOR CREATING PDF
+
+Prawn::Document.generate("#{title}.pdf") do
+	book.chapters.each do |c|
+		text "Chapter #{c.chapter_no}", 	:align => :center, :size => 30
+		move_down 20
+		c.text.each do |p|
+			text p
+			move_down 12
+		end
+		start_new_page()
+	end
+end
+
+# Prawn::Document.generate("hello.pdf") do
+# 	book.chapters.each do |chapter|
+# 		text chapter['heading'],    :align => :center, :size => 20
+# 		move_down 12
+# 		chapter.text.each do |p|
+# 			text p
+# 			move_down 8
+# 		end
+# 		start_new_page()
+# 	end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
